28ae9ec3ad77059a1e0985cc686603520177aafc
Aaron Martinez <aaronn.martinez@gmail.com>
2020-04-25 09:51:07 -0700

    Created additional repositories for this project.

--- src/main/java/com/aaronmartinez/lil/learningspring/DemoApplication.java
+++ src/main/java/com/aaronmartinez/lil/learningspring/DemoApplication.java
@@ -1,6 +1,10 @@
 package com.aaronmartinez.lil.learningspring;
 
+import com.aaronmartinez.lil.learningspring.data.entity.Guest;
+import com.aaronmartinez.lil.learningspring.data.entity.Reservation;
 import com.aaronmartinez.lil.learningspring.data.entity.Room;
+import com.aaronmartinez.lil.learningspring.data.repository.GuestRepository;
+import com.aaronmartinez.lil.learningspring.data.repository.ReservationRepository;
 import com.aaronmartinez.lil.learningspring.data.repository.RoomRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +31,30 @@ public class DemoApplication {
 		@GetMapping
 		public Iterable<Room> getRooms() {
 			return this.roomRepository.findAll();	
+		}
+	}
+
+	@RestController
+	@RequestMapping("/reservations")
+	public class ReservationController {
+		@Autowired
+		private ReservationRepository reservationRepository;
+
+		@GetMapping
+		public Iterable<Reservation> getReservations() {
+			return this.reservationRepository.findAll();
+		}
+	}
+
+	@RestController
+	@RequestMapping("/guests")
+	public class GuestController {
+		@Autowired
+		private GuestRepository guestRepository;
+
+		@GetMapping
+		public Iterable<Guest> getGuests() {
+			return this.guestRepository.findAll();
 		}
 	}
 }
---
+++ src/main/java/com/aaronmartinez/lil/learningspring/data/repository/GuestRepository.java
@@ -0,0 +1,11 @@
+package com.aaronmartinez.lil.learningspring.data.repository;
+
+import com.aaronmartinez.lil.learningspring.data.entity.Guest;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GuestRepository extends CrudRepository<Guest, Long>{
+    
+}
